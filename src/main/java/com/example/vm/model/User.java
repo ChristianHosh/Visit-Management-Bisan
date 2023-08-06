ed546ad91c2c1da34bf215fa5d3cbb733b31ab57
elianaellati <elianaellati2002@gmail.com>
2023-08-06 10:40:59 +0300

    created user model

--- src/main/java/com/example/vm/model/User.java
+++ src/main/java/com/example/vm/model/User.java
@@ -1,7 +1,6 @@
 package com.example.vm.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,5 +9,19 @@ import lombok.EqualsAndHashCode;
 @Table(name = "user_model")
 @EqualsAndHashCode(callSuper = true)
 public class User extends ModelAuditSuperclass {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "User_name")
+    private String username;
+
+    @Column(name = "Passwsord")
+    private String password;
+
+    @Column(name ="first_Name")
+    private String firstName;
+
+    @Column(name = "last_Name")
+    private String lastname;
+
 
 }
