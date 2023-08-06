03bed19cad03f7a80d2f5c9c068074d2652e8bb5
ChristianHosh <mctibhosh@gmail.com>
2023-08-06 10:52:39 +0300

    controller

--- src/main/java/com/example/vm/controller/UserController.java
+++ src/main/java/com/example/vm/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.example.vm.controller;
 
+import com.example.vm.model.User;
 import com.example.vm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -14,6 +20,11 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
