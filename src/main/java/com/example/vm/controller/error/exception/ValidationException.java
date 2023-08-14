7645a994468d4e1bbcaa0b66b3d4f3601a983d79
elianaellati <elianaellati2002@gmail.com>
2023-08-14 10:02:45 +0300

    address update

--- src/main/java/com/example/vm/controller/error/exception/ValidationException.java
+++ src/main/java/com/example/vm/controller/error/exception/ValidationException.java
@@ -3,6 +3,6 @@ package com.example.vm.controller.error.exception;
 public class ValidationException extends RuntimeException {
     public static final String NOT_Match = "Password Does Not match Confirm Password ";
     public ValidationException(String notMatch) {
-        super(notMatch)
+        super(notMatch);
     }
 }
