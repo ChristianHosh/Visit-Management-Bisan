2ef8d59f44b9ecb990d8a00886795fcdb1bdde59
ChristianHosh <mctibhosh@gmail.com>
2023-08-14 10:29:52 +0300

    CustomerService

--- src/main/java/com/example/vm/controller/CustomerController.java
+++ src/main/java/com/example/vm/controller/CustomerController.java
@@ -1,28 +1,15 @@
 package com.example.vm.controller;
 
-import com.example.vm.controller.error.exception.LocationNotFoundException;
-import com.example.vm.controller.error.exception.UserNotFoundException;
 import com.example.vm.dto.post.ContactPostDTO;
 import com.example.vm.dto.post.CustomerPostDTO;
-import com.example.vm.dto.UUIDDTO;
 import com.example.vm.dto.put.CustomerPutDTO;
-import com.example.vm.model.Contact;
-import com.example.vm.model.Customer;
-import com.example.vm.model.visit.VisitType;
 import com.example.vm.payload.detail.CustomerDetailPayload;
-import com.example.vm.payload.list.ContactListPayload;
-import com.example.vm.payload.list.CustomerListPayload;
-import com.example.vm.service.ContactService;
 import com.example.vm.service.CustomerService;
-import com.example.vm.service.VisitTypeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @CrossOrigin
@@ -31,124 +18,61 @@ import java.util.UUID;
 public class CustomerController {
 
     private final CustomerService customerService;
-    private final ContactService contactService;
-    private final VisitTypeService visitTypeService;
 
     @Autowired
-    public CustomerController(CustomerService customerService, ContactService contactService, VisitTypeService visitTypeService) {
+    public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
-        this.contactService = contactService;
-        this.visitTypeService = visitTypeService;
     }
 
     @GetMapping("")
-    public ResponseEntity<List<CustomerListPayload>> getAllCustomers() {
-        List<Customer> customerList = customerService.findAllCustomers();
-
-        return new ResponseEntity<>(toCustomerPayloadList(customerList), HttpStatus.OK);
+    public ResponseEntity<?> getAllCustomers() {
+        return customerService.findAllCustomers();
     }
 
     @GetMapping(value = "/search", params = "name")
-    public ResponseEntity<List<CustomerListPayload>> searchByCustomerName(@RequestParam("name") String name) {
-        List<Customer> customerList = customerService.searchByName(name);
-
-        return new ResponseEntity<>(toCustomerPayloadList(customerList), HttpStatus.OK);
+    public ResponseEntity<?> searchByCustomerName(@RequestParam("name") String name) {
+        return customerService.searchByName(name);
     }
 
     @GetMapping(value = "/search", params = "city")
-    public ResponseEntity<List<CustomerListPayload>> searchByCustomerCity(@RequestParam("city") String city) {
-        List<Customer> customerList = customerService.searchByAddressCity(city);
-
-        return new ResponseEntity<>(toCustomerPayloadList(customerList), HttpStatus.OK);
+    public ResponseEntity<?> searchByCustomerCity(@RequestParam("city") String city) {
+        return customerService.searchByAddressCity(city);
     }
 
     @GetMapping(value = "/search", params = "address")
-    public ResponseEntity<List<CustomerListPayload>> searchByCustomerAddress(@RequestParam("address") String address) {
-        List<Customer> customerList = customerService.searchByAddressLine(address);
-
-        return new ResponseEntity<>(toCustomerPayloadList(customerList), HttpStatus.OK);
+    public ResponseEntity<?> searchByCustomerAddress(@RequestParam("address") String address) {
+        return customerService.searchByAddressLine(address);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CustomerDetailPayload> getCustomerById(@PathVariable UUID id) {
-        Customer customer = customerService.findCustomerByUUID(id);
-
-        if (customer == null)
-            throw new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND);
-
-        return new ResponseEntity<>(customer.toDetailPayload(), HttpStatus.OK);
+        return customerService.findCustomerByUUID(id);
     }
 
     @GetMapping("/{id}/contacts")
-    public ResponseEntity<List<ContactListPayload>> getContactsByCustomerUUID(@PathVariable UUID id) {
-        Customer customer = customerService.findCustomerByUUID(id);
-
-        if (customer == null)
-            throw new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND);
-        List<Contact> contactList = customer.getContacts();
-        return new ResponseEntity<>(toContactPayloadList(contactList), HttpStatus.OK);
+    public ResponseEntity<?> getContactsByCustomerUUID(@PathVariable UUID id) {
+        return customerService.getCustomerContacts(id);
     }
 
     @PostMapping("")
-    public ResponseEntity<CustomerDetailPayload> saveNewCustomer(@RequestBody @Valid CustomerPostDTO customerRequest) {
-        Customer savedCustomer = customerService.saveNewCustomer(customerRequest);
-
-        if (savedCustomer == null)
-            throw new LocationNotFoundException();
-
-        return new ResponseEntity<>(savedCustomer.toDetailPayload(), HttpStatus.CREATED);
+    public ResponseEntity<?> saveNewCustomer(@RequestBody @Valid CustomerPostDTO customerRequest) {
+        return customerService.saveNewCustomer(customerRequest);
     }
 
     @PostMapping("/{id}/contacts")
-    public ResponseEntity<Contact> SaveContactToCustomerByUUID(@PathVariable UUID id, @RequestBody @Valid ContactPostDTO contactRequest) {
-        Customer customer = customerService.findCustomerByUUID(id);
-
-        if (customer == null)
-            throw new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND);
-
-        List<VisitType> visitTypes = new ArrayList<>();
-
-        for (UUIDDTO uuiddto : contactRequest.getTypes()){
-            VisitType visitType = visitTypeService.findById(uuiddto.getUuid());
-
-            if (visitType == null)
-                throw new UserNotFoundException(UserNotFoundException.TYPE_NOT_FOUND);
-
-            visitTypes.add(visitType);
-        }
-
-        Contact savedContact = contactService.saveNewContact(customer, contactRequest, visitTypes);
-
-        return new ResponseEntity<>(savedContact, HttpStatus.OK);
+    public ResponseEntity<?> SaveContactToCustomerByUUID(@PathVariable UUID id, @RequestBody @Valid ContactPostDTO contactRequest) {
+        return customerService.saveContactToCustomer(id, contactRequest);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CustomerDetailPayload> updateCustomer(@PathVariable UUID id, @RequestBody @Valid CustomerPutDTO customerRequest) {
-        Customer customerToUpdate = customerService.findCustomerByUUID(id);
-        if (customerToUpdate == null)
-            throw new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND);
-
-        Customer updatedCustomer = customerService.updateCustomer(customerToUpdate, customerRequest);
-
-        return new ResponseEntity<>(updatedCustomer.toDetailPayload(), HttpStatus.OK);
+    public ResponseEntity<?> updateCustomer(@PathVariable UUID id, @RequestBody @Valid CustomerPutDTO customerRequest) {
+        return customerService.updateCustomer(id, customerRequest);
     }
 
     @PutMapping("/{id}/endis")
-    public ResponseEntity<CustomerDetailPayload> enableCustomer(@PathVariable UUID id) {
-        Customer customerToEnable = customerService.findCustomerByUUID(id);
-
-        if (customerToEnable == null)
-            throw new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND);
-
-        customerToEnable = customerService.enableCustomer(customerToEnable);
-
-        return new ResponseEntity<>(customerToEnable.toDetailPayload(), HttpStatus.OK);
+    public ResponseEntity<?> enableCustomer(@PathVariable UUID id) {
+        return customerService.enableCustomer(id);
     }
 
-    private static List<CustomerListPayload> toCustomerPayloadList(List<Customer> customerList) {
-        return customerList.stream().map(Customer::toListPayload).toList();
-    }
-    private static List<ContactListPayload> toContactPayloadList(List<Contact> contactList) {
-        return contactList.stream().map(Contact::toListPayload).toList();
-    }
+
 }
--- src/main/java/com/example/vm/service/CustomerService.java
+++ src/main/java/com/example/vm/service/CustomerService.java
@@ -1,22 +1,30 @@
 package com.example.vm.service;
 
+import com.example.vm.controller.error.exception.LocationNotFoundException;
+import com.example.vm.controller.error.exception.UserNotFoundException;
 import com.example.vm.dto.post.AddressPostDTO;
+import com.example.vm.dto.post.ContactPostDTO;
 import com.example.vm.dto.post.CustomerPostDTO;
 import com.example.vm.dto.put.AddressPutDTO;
 import com.example.vm.dto.put.CustomerPutDTO;
 import com.example.vm.model.Address;
+import com.example.vm.model.Contact;
 import com.example.vm.model.Customer;
+import com.example.vm.payload.detail.CustomerDetailPayload;
+import com.example.vm.payload.list.ContactListPayload;
+import com.example.vm.payload.list.CustomerListPayload;
 import com.example.vm.repository.CustomerRepository;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -31,30 +39,45 @@ public class CustomerService {
         this.repository = repository;
     }
 
-    public List<Customer> findAllCustomers() {
-
-        return repository.findAll();
+    public ResponseEntity<List<CustomerListPayload>> findAllCustomers() {
+        return ResponseEntity.ok(toCustomerPayloadList(repository.findAll()));
     }
 
-    public Customer findCustomerByUUID(UUID uuid) {
-        Optional<Customer> customerOptional = repository.findById(uuid);
+    public ResponseEntity<CustomerDetailPayload> findCustomerByUUID(UUID id) {
+        Customer foundCustomer = repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(UserNotFoundException.USER_NOT_FOUND));
 
-        return customerOptional.orElse(null);
+        return ResponseEntity.ok(foundCustomer.toDetailPayload());
     }
 
-    public List<Customer> searchByName(String name) {
-        return repository.searchCustomersByNameContaining(name);
+    public ResponseEntity<List<CustomerListPayload>> searchByName(String name) {
+        List<Customer> customerList = repository.searchCustomersByNameContaining(name);
+
+        return ResponseEntity.ok(toCustomerPayloadList(customerList));
     }
 
-    public List<Customer> searchByAddressCity(String city) {
-        return repository.searchCustomersByAddress_CityContaining(city);
+    public ResponseEntity<List<CustomerListPayload>> searchByAddressCity(String city) {
+        List<Customer> customerList = repository.searchCustomersByAddress_CityContaining(city);
+
+        return ResponseEntity.ok(toCustomerPayloadList(customerList));
     }
 
-    public List<Customer> searchByAddressLine(String addressLine) {
-        return repository.searchCustomersByAddress_AddressLine1ContainingOrAddress_AddressLine2Containing(addressLine, addressLine);
+    public ResponseEntity<List<CustomerListPayload>> searchByAddressLine(String addressLine) {
+        List<Customer> customerList = repository.searchCustomersByAddress_AddressLine1ContainingOrAddress_AddressLine2Containing(addressLine, addressLine);
+
+        return ResponseEntity.ok(toCustomerPayloadList(customerList));
     }
 
-    public Customer saveNewCustomer(CustomerPostDTO customerRequest) {
+    public ResponseEntity<List<ContactListPayload>> getCustomerContacts(UUID id) {
+        Customer foundCustomer = repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND));
+
+        List<Contact> contactList = foundCustomer.getContacts();
+
+        return ResponseEntity.ok(toContactPayloadList(contactList));
+    }
+
+    public ResponseEntity<CustomerDetailPayload> saveNewCustomer(CustomerPostDTO customerRequest) {
         Timestamp timestamp = Timestamp.from(Instant.now());
 
         AddressPostDTO addressRequest = customerRequest.getAddress();
@@ -79,23 +102,52 @@ public class CustomerService {
             customerToSave.setCreatedTime(timestamp);
             customerToSave.setLastModifiedTime(timestamp);
 
-            return repository.save(customerToSave);
+            customerToSave = repository.save(customerToSave);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(customerToSave.toDetailPayload());
 
         } catch (Exception e) {
             System.out.println("ERROR " + e.getMessage());
         }
 
-        return null;
+        throw new LocationNotFoundException();
     }
 
-    public Customer updateCustomer(Customer customerToUpdate, CustomerPutDTO updatedDTO) {
+
+    public ResponseEntity<CustomerDetailPayload> saveContactToCustomer(UUID id, ContactPostDTO contactRequest) {
+        Customer foundCustomer = repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND));
+
+        String formattedNumber = contactRequest.getPhoneNumber();
+
+        contactRequest.setPhoneNumber(formattedNumber);
+
+        Contact newContact = Contact.builder()
+                .firstName(contactRequest.getFirstName())
+                .lastName(contactRequest.getLastName())
+                .email(contactRequest.getEmail())
+                .phoneNumber(contactRequest.getPhoneNumber())
+                .enabled(1)
+                .build();
+
+        foundCustomer.getContacts().add(newContact);
+
+        foundCustomer = repository.save(foundCustomer);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(foundCustomer.toDetailPayload());
+    }
+
+    public ResponseEntity<CustomerDetailPayload> updateCustomer(UUID id, CustomerPutDTO customerRequest) {
+        Customer customerToUpdate = repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND));
+
         Timestamp timestamp = Timestamp.from(Instant.now());
 
-        AddressPutDTO addressRequest = updatedDTO.getAddress();
+        AddressPutDTO addressRequest = customerRequest.getAddress();
 
         Address oldAddress = customerToUpdate.getAddress();
 
-        customerToUpdate.setName(updatedDTO.getName() == null ? customerToUpdate.getName() : updatedDTO.getName());
+        customerToUpdate.setName(customerRequest.getName());
         customerToUpdate.setAddress(Address.builder()
                 .addressLine1(addressRequest.getAddressLine1())
                 .addressLine2(addressRequest.getAddressLine2())
@@ -111,15 +163,28 @@ public class CustomerService {
             customerToUpdate.getAddress().setLastModifiedTime(timestamp);
             customerToUpdate.getAddress().setCreatedTime(oldAddress.getCreatedTime());
 
-            return repository.save(customerToUpdate);
+            customerToUpdate = repository.save(customerToUpdate);
+
+            return ResponseEntity.ok(customerToUpdate.toDetailPayload());
         } catch (Exception e) {
             System.out.println("ERROR " + e.getMessage());
         }
 
-        return null;
+        throw new LocationNotFoundException();
     }
 
-    private void setLngLat(Customer customerToUpdate, String addressLine1, String addressLine2, String city, String zipcode) throws com.google.maps.errors.ApiException, InterruptedException, java.io.IOException {
+    public ResponseEntity<CustomerDetailPayload> enableCustomer(UUID id) {
+        Customer foundCustomer = repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(UserNotFoundException.CUSTOMER_NOT_FOUND));
+
+        foundCustomer.setEnabled(foundCustomer.getEnabled() == 0 ? 1 : 0);
+
+        foundCustomer = repository.save(foundCustomer);
+
+        return ResponseEntity.ok(foundCustomer.toDetailPayload());
+    }
+
+    private static void setLngLat(Customer customerToUpdate, String addressLine1, String addressLine2, String city, String zipcode) throws com.google.maps.errors.ApiException, InterruptedException, java.io.IOException {
         GeoApiContext context = new GeoApiContext.Builder()
                 .apiKey(GEOLOCATION_KEY)
                 .build();
@@ -138,10 +203,12 @@ public class CustomerService {
     }
 
 
-    public Customer enableCustomer(Customer customer) {
-        customer.setEnabled(customer.getEnabled() == 0 ? 1 : 0);
-        return repository.save(customer);
+    private static List<CustomerListPayload> toCustomerPayloadList(List<Customer> customerList) {
+        return customerList.stream().map(Customer::toListPayload).toList();
     }
 
+    private static List<ContactListPayload> toContactPayloadList(List<Contact> contactList) {
+        return contactList.stream().map(Contact::toListPayload).toList();
+    }
 
 }
