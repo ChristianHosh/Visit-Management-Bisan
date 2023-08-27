ccf6b11036ff7c18a6f67bbab7210f338154cb25
ChristianHosh <mctibhosh@gmail.com>
2023-08-27 10:33:17 +0300

    errors

--- src/main/java/com/example/vm/controller/error/exception/UserAlreadyExistsException.java
+++ src/main/java/com/example/vm/controller/error/exception/UserAlreadyExistsException.java
@@ -1,11 +1,11 @@
 package com.example.vm.controller.error.exception;
 
+import com.example.vm.controller.error.ErrorMessages;
+
 public class UserAlreadyExistsException extends RuntimeException{
 
-    private static final String ALREADY_EXISTS_MESSAGE = "Invalid Username: Already Exists";
-
     public UserAlreadyExistsException() {
-        super(ALREADY_EXISTS_MESSAGE);
+        super(ErrorMessages.USER_ALREADY_EXISTS);
     }
 
 }
